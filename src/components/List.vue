f7e60864983f7dceb2a87ec4a408ec081e950aaa
Polina Shneider <beatrixlinkoln@gmail.com>
2020-02-11 00:45:01 +0300

    Added users layout

---
+++ src/components/List.vue
@@ -0,0 +1,83 @@
+<template>
+    <div>
+        <ul class="list">
+            <li class="list__item" v-for="(user, i) in users" :key="i">
+              <span v-for="(value, name, index) in user" :key="index">
+                <label>{{getLabel(name)}}:</label> {{value}}<br>
+              </span>
+                <i class="fas fa-pencil-alt" v-on:click="edit($event, user)"></i>
+                <DialogContainer :visible="visible">
+                    <Dialog></Dialog>
+                </DialogContainer>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    import Dialog from "./Dialog";
+    import DialogContainer from "./DialogContainer";
+
+    export default {
+        name: 'List',
+        props: {
+            msg: String,
+            users: Array,
+            keyword: String
+        },
+        data: () => {
+            return {
+                isEdited: null
+            }
+        },
+        components: {
+            Dialog,
+            DialogContainer
+        },
+        methods: {
+            getLabel: function (key) {
+                return key /*this.$dictionary[this.keyword][key]*/
+            },
+            edit: function(event, item) {
+                console.log(event, item)
+            }
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    * {
+        text-align: initial;
+    }
+
+    .list {
+        border-radius: 4px;
+        border: 1px solid sandybrown;
+        padding: 8px;
+        margin: 10px;
+    }
+
+    .list__item {
+        display: flex;
+        justify-content: space-between;
+        padding: 8px;
+        border: 1px solid sandybrown;
+    }
+
+    .list__item:not(:first-of-type) {
+        border-top: 0;
+    }
+
+    .list__item span {
+        margin-right: 20px;
+    }
+
+    .list__item i {
+        cursor: pointer;
+    }
+
+    .list__item label {
+        font-weight: bold;
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './styles/main.css'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
+
+Vue.prototype.$dictionary = {
+  'users': {
+    name: 'Имя',
+    surName: 'Фамилия',
+    login: 'Логин'
+  }
+};
